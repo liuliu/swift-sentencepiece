0c950d4f0a6f71ab5d8310691b2ed07a2f1dd635
Weiyan <weiyanlin117@gmail.com>
2026-01-08 16:46:04 -0800

    support SPM (#1)
    * support swift package
    
    * clean up and update path
    
    * update swift-package-wrapper-sentencepiece
    
    * update package name
    
    * remove unnecessary modulemap

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "swift-sentencepiece",
+    products: [
+        .library(
+            name: "SentencePiece",
+            targets: ["SentencePiece"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/weiyanlin117/swift-package-support-sentencepiece", revision: "ca13a7cc8c8bf8730d122340d2218bbe8ed274dc"),
+    ],
+    targets: [
+        .target(
+            name: "SentencePiece",
+            dependencies: ["C_sentencepiece"],
+            path: "Sources",
+            exclude: ["BUILD.bazel", "C_sentencepiece"],
+            sources: ["SentencePiece.swift"]
+        ),
+        .target(
+            name: "C_sentencepiece",
+            dependencies: [
+                .product(name: "sentencepiece", package: "swift-package-support-sentencepiece"),
+            ],
+            path: "Sources/C_sentencepiece",
+            publicHeadersPath: ".",
+            cxxSettings: [
+                .headerSearchPath("."),
+            ]
+        ),
+    ],
+    cxxLanguageStandard: .cxx17
+)
