8d17bf2e017c97563e8805545d676be9739b6c0e
Weiyan <weiyanlin117@gmail.com>
2026-02-24 10:17:22 -0800

    update swift-package-support-sentencepiece to 94f570e (#2)
    * update swift-package-support-sentencepiece to 94f570e
    
    - Bump revision to pick up sentencepiece → sentencepiece_cpp rename
    - Update product dependency name accordingly (sentencepiece_cpp)
    
    The rename fixes Xcode module collision where the SPM target name
    'sentencepiece' conflicted with the Clang module of the same name.
    
    * update swift-package-support-sentencepiece to 2ff2787 (remove duplicate src/module.modulemap)
    
    * update sentencepiece_cpp as swift-package-support-sentencepiece

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/weiyanlin117/swift-package-support-sentencepiece", revision: "ca13a7cc8c8bf8730d122340d2218bbe8ed274dc"),
+        .package(url: "https://github.com/weiyanlin117/swift-package-support-sentencepiece", revision: "a39a5be0b3e3ad9bcb19b085af7dd891c00aa3d2"),
     ],
     targets: [
         .target(
@@ -23,7 +23,7 @@ let package = Package(
         .target(
             name: "C_sentencepiece",
             dependencies: [
-                .product(name: "sentencepiece", package: "swift-package-support-sentencepiece"),
+                .product(name: "swift-package-support-sentencepiece", package: "swift-package-support-sentencepiece"),
             ],
             path: "Sources/C_sentencepiece",
             publicHeadersPath: ".",
